09e33973c0e6027d3c52bc406cd9717331163765
TeaTable <jianyeliu1988@gmail.com>
2013-12-05 20:57:56 -0500

    Update IntegerToRoman.cpp

--- IntegerToRoman.cpp
+++ IntegerToRoman.cpp
@@ -28,3 +28,37 @@ string intToRoman(int num){
   
   return result;
 }
+
+string intToRoman2(int num){
+    string tag = {"I", "V", "X", "L", "C", "D", "M"};
+    int pivot = 1, index = 0;
+    while (num / pivot > 9) {
+        pivot*=10;
+        index+=2;
+    }
+    string result = "";
+    while (num > 0){
+        int digit = num / pivot;
+        if ( digit >= 1 && digit < 4){
+            result += tag[index];
+            num -= pivot;
+            continue;
+        }
+        else if ( digit == 4){
+            result += tag[index] + tag[inde+1];
+            num -= 4*pivot;
+        }
+        else if ( digit >= 5 && digit < 9){
+            result += tag[index+1];
+            num -= pivot*5;
+            continue;
+        }
+        else if ( digit == 9){
+            result += tag[index] + tag[index+2];
+            num -= pivot*9;
+        }
+        pivot /= 10;
+        index -= 2;
+    }
+    return result;
+}
