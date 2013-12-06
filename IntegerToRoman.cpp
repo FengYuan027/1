fc5b75586e0d2272ce6dcdf0bb05ce954a93ef66
TeaTable <jianyeliu1988@gmail.com>
2013-12-05 20:31:36 -0500

    Create IntegerToRoman.cpp

---
+++ IntegerToRoman.cpp
@@ -0,0 +1,30 @@
+// The number is between 1 and 3999
+
+string intToRoman(int num){
+  string tag[] = {"I", "V", "X", "L", "C", "D", "M"};
+  
+  string result = "";
+  int index = 0;
+  
+  while (num > 0) {
+    int digit = num % 10;
+    if (digit > 0 && digit < 4 || digit > 5 && digit <9){
+      result = "" + tag[index] + result;
+      num--;
+      continue;
+    }
+    else if (digit == 4) {
+      result = "" + tag[index] + tag[index+1] + result;
+    }
+    else if (index == 5)
+      result = "" + tag[index+1] + result;
+    }
+    else if (index == 9) {
+      result = "" + tag[index] + tag[index+2] + result;
+    }
+    num /= 10;
+    index += 2;
+  }
+  
+  return result;
+}
