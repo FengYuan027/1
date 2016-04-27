8c9a550632f30f487887c5577d636ab9eb8d93b3
TeaTable <jianyeliu1988@gmail.com>
2016-04-27 14:11:18 -0700

    Create Remove Duplicates from Sorted Array.java

---
+++ Iterate/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int current = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[current]) {
+                nums[++current] = nums[i];
+            }
+        }
+        return current+1;
+    }
+}
