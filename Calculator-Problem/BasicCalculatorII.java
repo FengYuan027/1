cf1708548cfd13c80f55d866714915568fd5aa84
TeaTable <jianyeliu1988@gmail.com>
2016-04-26 22:28:08 -0700

    Update BasicCalculatorII.java

--- Calculator-Problem/BasicCalculatorII.java
+++ Calculator-Problem/BasicCalculatorII.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
 	public int calculate(String input) {
 		Stack<Integer> s = new Stack<Integer>();
 		int current = 0, result = 0, sign = 1;
@@ -28,4 +28,30 @@ public class Solution {
 		}
 		return result + current * sign;
 	}
+}
+
+public class Solution2 {
+    public int calculate(String input) {
+        int result = 0, current = 0, sign = 1;
+        Stack<Integer> s = new Stack<Integer>();
+        s.push(sign);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c >= '0' && c <= '9') {
+                current = current * 10 + (c - '0');
+            }
+            else if (c == '+' || c == '-') {
+                result += current * sign;
+                current = 0;
+                sign = s.peek() * (c == '+' ? 1 : -1);
+            }
+            else if (c == '(') {
+                s.push(sign);
+            }
+            else if (c == ')') {
+                s.pop(); // Don't need to compute since the effect to sign is global
+            }
+        }
+        return result + current * sign;
+    }
 }
