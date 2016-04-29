c599599c43837b2e412728283be591061cbd1136
TeaTable <jianyeliu1988@gmail.com>
2016-04-28 22:41:37 -0700

    Update Search a 2D matrix.java

--- BinarySearch/Search a 2D matrix.java
+++ BinarySearch/Search a 2D matrix.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public boolean searchMatrix(int[][] matrix, int target) {
         int start = 0, end = matrix.length - 1;
         while (start <= end) {
@@ -16,6 +16,19 @@ public class Solution {
             if (matrix[row][mid] == target) return true;
             else if (matrix[row][mid] > target) end = mid - 1;
             else start = mid + 1;
+        }
+        return false;
+    }
+}
+
+public class Solution2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int M = matrix.length, N = matrix[0].length, start = 0, end = M*N-1;
+        while (start <= end) {
+            int mid = (start + end) / 2, val = matrix[mid/N][mid%N];
+            if (val == target) return true;
+            else if (val < target) start = mid + 1;
+            else end = mid - 1;
         }
         return false;
     }
