8b230fcb82915e987f1d73cd97d3b6c065830204
TeaTable <jianyeliu1988@gmail.com>
2016-04-30 06:28:21 -0700

    Update Edit Distance.java

--- DynamicProgramming/Edit Distance.java
+++ DynamicProgramming/Edit Distance.java
@@ -13,3 +13,22 @@ public class Solution1 {
         return distance[M][N];
     }
 }
+
+public class Solution2 {
+    public int minDistance(String word1, String word2) {
+        int M = word1.length(), N = word2.length();
+        int[] distance = new int[N+1];
+        for (int i = 1; i <= N; i++) distance[i] = i;
+        for (int i = 1; i <= M; i++) {
+            int pre = distance[0];
+            distance[0] = i;
+            for (int j = 1; j <= N; j++) {
+                int temp = distance[j];
+                if (word1.charAt(i-1) == word2.charAt(j-1)) distance[j] = pre;
+                else distance[j] = Math.min(distance[j-1], Math.min(distance[j], pre)) + 1;
+                pre = temp;
+            }
+        }
+        return distance[N];
+    }
+}
