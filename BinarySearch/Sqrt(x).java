005ebaf24d38a67d231ba2a9e1534cba836b88b3
TeaTable <jianyeliu1988@gmail.com>
2016-04-30 23:21:25 -0700

    Update Sqrt(x).java

--- BinarySearch/Sqrt(x).java
+++ BinarySearch/Sqrt(x).java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int mySqrt(int x) {
         if (x < 1) return 0;
         int start = 1, end = Math.max(1, Math.min((int)Math.sqrt(Integer.MAX_VALUE), x/2));
@@ -9,5 +9,20 @@ public class Solution {
             else end = mid - 1;
         }
         return start - 1;
+    }
+}
+
+public class Solution2 {
+    public int mySqrt(int x) {
+        if (x < 1) return 0;
+        int start = 1, end = Integer.MAX_VALUE;
+        while (true) {
+            int mid = start + (end - start) / 2;
+            if (mid > x / mid) end = mid - 1;
+            else {
+                if (mid + 1 > x / (mid + 1)) return mid;
+                else start = mid + 1;
+            }
+        }
     }
 }
