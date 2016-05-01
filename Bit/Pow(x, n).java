c0db3937dbc6ae370e4ca4042b7b2af3928005fd
TeaTable <jianyeliu1988@gmail.com>
2016-05-01 12:14:16 -0700

    Update Pow(x, n).java

--- Bit/Pow(x, n).java
+++ Bit/Pow(x, n).java
@@ -36,3 +36,20 @@ public class Solution2 {
         return n%2 == 0 ? myPow(x*x, n/2) : x * myPow(x*x, n/2);
     }
 }
+
+class Solution3 {
+    double myPow(double x, int n) { 
+        if(n==0) return 1;
+        if(n<0) {
+            n = -n;
+            x = 1/x;
+        }
+        double ans = 1;
+        while(n>0){
+            if(n&1) ans *= x;
+            x *= x;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
