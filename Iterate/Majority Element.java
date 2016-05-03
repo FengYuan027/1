d3f0380072693caa5584902a50a484905fa173f3
TeaTable <jianyeliu1988@gmail.com>
2016-05-02 21:15:35 -0700

    Create Majority Element.java

---
+++ Iterate/Majority Element.java
@@ -0,0 +1,20 @@
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                majority = num;
+                count = 1;
+            }
+            else if (majority == num) count++;
+            else count--;
+        }
+        return majority;
+    }
+}
