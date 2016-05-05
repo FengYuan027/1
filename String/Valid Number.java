4c7b486de1c969b8348da65af6ac47562ad6e6de
TeaTable <jianyeliu1988@gmail.com>
2016-05-04 18:42:44 -0700

    Update Valid Number.java

--- String/Valid Number.java
+++ String/Valid Number.java
@@ -45,3 +45,51 @@ public class Solution1 {
         return hasNumber && hasENumber;
     }
 }
+
+public class Solution2 {
+    public boolean isNumber(String s) {
+        int index = 0;
+        char[] number = s.toCharArray();
+        while (index < number.length && isSpace(number[index])) {
+            index++;
+        }
+        if (index < number.length && isSign(number[index])) {
+            index++;
+        }
+        boolean hasNumber = false;
+        while (index < number.length && isDigit(number[index])) {
+            hasNumber = true;
+            index++;
+        }
+        if (index < number.length && number[index] == '.') {
+            index++;
+        }
+        while (index < number.length && isDigit(number[index])) {
+            hasNumber = true;
+            index++;
+        }
+        if (hasNumber && index < number.length && isE(number[index])) {
+            hasNumber = false;
+            index++;
+            if (index < number.length && isSign(number[index])) {
+                index++;
+            }
+            while (index < number.length && isDigit(number[index])) {
+                index++;
+                hasNumber = true;
+            }
+        }
+        while (index < number.length && isSpace(number[index])) {
+            index++;
+        }
+        return index == number.length && hasNumber;
+    }
+    
+    private boolean isDigit (char c) { return c >= '0' && c <= '9';}
+    
+    private boolean isSign(char c) { return c == '-' || c == '+'; }
+    
+    private boolean isE(char c) { return c == 'e'; }
+    
+    private boolean isSpace(char c) { return c == ' ';}
+}
