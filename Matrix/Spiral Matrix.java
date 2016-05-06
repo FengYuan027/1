0ac5e8c474f31766592712749aff8e36fea0994a
TeaTable <jianyeliu1988@gmail.com>
2016-05-05 19:18:33 -0700

    Update Spiral Matrix.java

--- Matrix/Spiral Matrix.java
+++ Matrix/Spiral Matrix.java
@@ -1,6 +1,47 @@
+/*
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
 
+For example,
+Given the following matrix:
 
-public class Solution {
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+
+You should return [1,2,3,6,9,8,7,4,5]. 
+*/
+
+public class Solution1 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> nums = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return nums;
+        int M = matrix.length, N = matrix[0].length, minD = Math.min(M, N);
+        for (int i = 0; i < minD/2 + minD%2; i++) {
+            int left = i, right = N - 1 - i, top = i, down = M - 1 - i;
+            for (int j = left; j <= right; j++) {
+                nums.add(matrix[top][j]);
+            }
+            for (int j = top + 1; j <= down; j++) {
+                nums.add(matrix[j][right]);
+            }
+            if (down > top) {
+                for (int j = right - 1; j >= left; j--) {
+                    nums.add(matrix[down][j]);
+                }
+            }
+            if (right > left) {
+                for (int j = down - 1; j > top; j--) {
+                    nums.add(matrix[j][left]);
+                }
+            }
+        }
+        return nums;
+    }
+}
+
+public class Solution2 {
     public List<Integer> spiralOrder(int[][] matrix) {
 
         List<Integer> res = new ArrayList<Integer>();
