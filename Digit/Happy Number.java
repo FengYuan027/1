648ea4f61423c64843a044644cd040477cc79200
TeaTable <jianyeliu1988@gmail.com>
2016-05-08 18:45:09 -0700

    Create Happy Number.java

---
+++ Digit/Happy Number.java
@@ -0,0 +1,33 @@
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+
+public class Solution {
+    public boolean isHappy(int n) {
+        if (n <= 0) return false;
+        int slow = n, fast = next(n);
+        while (slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        return slow == 1;
+    }
+    
+    private int next(int n) {
+        int happy = 0;
+        while (n > 0) {
+            happy += (n%10)*(n%10);
+            n /= 10;
+        }
+        return happy;
+    }
+}
