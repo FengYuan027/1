f5262a935b8ac862c4888512ab6fab3618fe86ad
TeaTable <jianyeliu1988@gmail.com>
2016-05-12 14:32:51 -0700

    Update Flatten BST to Doubly Linked List.java

--- BinaryTree/Flatten BST to Doubly Linked List.java
+++ BinaryTree/Flatten BST to Doubly Linked List.java
@@ -1,2 +1,19 @@
 
-class S
+class Solution {
+  
+  private TreeNode prev = null;
+  
+  public TreeNode flatten (TreeNode root) {
+    if (node == null) return null;
+    flatten(root.right);
+    TreeNode left = root.left;
+    root.right = prev;
+    if (prev != null) {
+      prev.left = root;
+    }
+    root.left = null;
+    prev = root;
+    if (left != null) return flatten(left);
+    return root;
+  }
+}
