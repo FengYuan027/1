cdc3fb6c833b51c172d4c1afc02f1213defb737e
TeaTable <jianyeliu1988@gmail.com>
2016-05-14 13:55:32 -0700

    Create Excel Sheet Column Number.java

---
+++ Math/Excel Sheet Column Number.java
@@ -0,0 +1,25 @@
+/*
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+*/
+
+public class Solution {
+    public int titleToNumber(String s) {
+        int number = 0;
+        for (char c : s.toCharArray()) {
+            number = number * 26 + c - 'A' + 1;
+        }
+        return number;
+    }
+}
