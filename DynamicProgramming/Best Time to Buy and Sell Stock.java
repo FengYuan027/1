709f14eae5da0fae5259ee08844f0efd1c4f0d3d
TeaTable <jianyeliu1988@gmail.com>
2016-05-19 18:29:45 -0700

    Update Best Time to Buy and Sell Stock.java

--- DynamicProgramming/Best Time to Buy and Sell Stock.java
+++ DynamicProgramming/Best Time to Buy and Sell Stock.java
@@ -1,5 +1,5 @@
 // I
-public class Solution {
+public class SolutionI {
     public int maxProfit(int[] prices) {
         int currentMin = Integer.MAX_VALUE, maxProfit = 0;
         for (int i = 0; i < prices.length; i++) {
@@ -7,5 +7,16 @@ public class Solution {
             currentMin = Math.min(currentMin, prices[i]);
         }
         return maxProfit;
+    }
+}
+
+// II
+public class SolutionII {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            profit += Math.max(0, prices[i] - prices[i-1]);
+        }
+        return profit;
     }
 }
